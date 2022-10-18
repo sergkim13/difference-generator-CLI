c4d29fe37b13655274be06ab0b18e2a944a03f6a
Sergey Kim <sergkim7@gmail.com>
2022-10-18 22:42:32 +0300

    upd gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -4,5 +4,5 @@ def gendiff():
     gendiff = argparse.ArgumentParser()
     gendiff.add_argument('first_file')
     gendiff.add_argument('second_file')
-    args = parser.parse_args()
+    args = gendiff.parse_args()
     print(args.echo)
