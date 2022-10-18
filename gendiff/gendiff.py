050ff3daa4c70a456d443ee516bde75700992263
Sergey Kim <sergkim7@gmail.com>
2022-10-18 23:07:39 +0300

    upd gendiff.py

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,8 +1,9 @@
 import argparse
 
 def gendiff():
-    gendiff = argparse.ArgumentParser()
+    gendiff = argparse.ArgumentParser(description = 'Compares two configuration files and shows a difference.')
     gendiff.add_argument('first_file')
     gendiff.add_argument('second_file')
+    gendiff.add_argument('-f', '--format', help = 'set format of output')
     args = gendiff.parse_args()
     print(args.echo)
