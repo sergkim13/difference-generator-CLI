a9feceea78f77a5510b2f3a5d41f26a09e3d4708
Sergey Kim <sergkim7@gmail.com>
2022-10-19 15:51:45 +0300

    Revert "upd gendiff.py"
    This reverts commit f909e3a3a51c50ae91ac0dfa31bad02976e0ba25.

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -27,3 +27,11 @@ file1 = 'gendiff/file1.json'
 file2 = 'gendiff/file2.json'
 
 generate_diff(file1, file2)
+
+# def gendiff():
+#     gendiff = argparse.ArgumentParser(description = 'Compares two configuration files and shows a difference.')
+#     gendiff.add_argument('first_file')
+#     gendiff.add_argument('second_file')
+#     gendiff.add_argument('-f', '--format', help = 'set format of output')
+#     args = gendiff.parse_args()
+#     print(args.echo)
