c14c158010487025ddf832724f5a765052abd4a8
Sergey Kim <sergkim7@gmail.com>
2022-10-20 22:21:46 +0300

    fix linter

--- gendiff_pack/gendiff_engine/gendiff_script_body.py
+++ gendiff_pack/gendiff_engine/gendiff_script_body.py
@@ -1,6 +1,7 @@
 import argparse
 from gendiff_pack import generate_diff
 
+
 def gendiff_script():
     gendiff = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.'
