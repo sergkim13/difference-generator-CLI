9d294d59fcdc0914f630fe65b5ebfc8c2d4bfd7f
Sergey Kim <sergkim7@gmail.com>
2022-10-25 20:14:17 +0300

    add tests with .yml and .yaml

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,11 +1,15 @@
 from gendiff_pack.gendiff_engine.gendiff import generate_diff
 
 
-file1 = 'tests/fixtures/file1.json'
-file2 = 'tests/fixtures/file2.json'
-
+file1_json = 'tests/fixtures/file1.json'
+file2_json = 'tests/fixtures/file2.json'
+file1_yml = 'tests/fixtures/file1.yml'
+file2_yaml = 'tests/fixtures/file2.yaml'
 
 def test_generate_diff():
   with open('tests/fixtures/result.txt') as f:
     result = f.read()
-    assert generate_diff(file1, file2) == result    
+    assert generate_diff(file1_json, file2_json) == result    
+    assert generate_diff(file1_yml, file2_yaml) == result
+    assert generate_diff(file1_yml, file2_json) == result
+    assert generate_diff(file1_json, file2_yaml) == result
