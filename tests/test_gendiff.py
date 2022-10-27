a450668c8055b0b9624f03b69089000ae64b4425
Sergey Kim <sergkim7@gmail.com>
2022-10-27 20:27:22 +0300

    restructured tests directory

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,13 +1,13 @@
 from gendiff_pack.gendiff_engine.gendiff import generate_diff
 
 
-file1_json = 'tests/fixtures/file1.json'
-file2_json = 'tests/fixtures/file2.json'
-file1_yml = 'tests/fixtures/file1.yml'
-file2_yaml = 'tests/fixtures/file2.yaml'
+file1_json = 'tests/fixtures/plain/file1.json'
+file2_json = 'tests/fixtures/plain/file2.json'
+file1_yml = 'tests/fixtures/plain/file1.yml'
+file2_yaml = 'tests/fixtures/plain/file2.yaml'
 
-def test_generate_diff():
-  with open('tests/fixtures/result.txt') as f:
+def test_plain_generate_diff():
+  with open('tests/fixtures/plain/result.txt') as f:
     result = f.read()
     assert generate_diff(file1_json, file2_json) == result    
     assert generate_diff(file1_yml, file2_yaml) == result
