4115b3d03c1cf292157d60fa2c1fad5bca445c92
Sergey Kim <sergkim7@gmail.com>
2022-10-28 21:19:16 +0300

    upd test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -6,6 +6,7 @@ file2_json = 'tests/fixtures/plain/file2.json'
 file1_yml = 'tests/fixtures/plain/file1.yml'
 file2_yaml = 'tests/fixtures/plain/file2.yaml'
 
+
 def test_plain_generate_diff():
   with open('tests/fixtures/plain/result.txt') as f:
     result = f.read()
