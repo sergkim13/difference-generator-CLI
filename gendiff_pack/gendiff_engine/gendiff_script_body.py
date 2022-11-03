5aba7b6cc7d3d13bab2375be4d4e3e33a6e4857d
Sergey Kim <sergkim7@gmail.com>
2022-11-03 19:04:45 +0300

    upd gendiff_script_body

--- gendiff_pack/gendiff_engine/gendiff_script_body.py
+++ gendiff_pack/gendiff_engine/gendiff_script_body.py
@@ -1,12 +1,18 @@
 import argparse
 from gendiff_pack import generate_diff
-
+from gendiff_pack.gendiff_engine.gendiff import generate_diff_dict
+from gendiff_pack.gendiff_engine.input_format_parser import parse_input_file
+from gendiff_pack.gendiff_format.format_diff import format_diff_stylish
 
 def gendiff_script():
     gendiff = argparse.ArgumentParser(
         description='Compares two configuration files and shows a difference.')
     gendiff.add_argument('first_file')
     gendiff.add_argument('second_file')
-    gendiff.add_argument('-f', '--format', help='set format of output')
+    gendiff.add_argument('-f', '--format', help='set format of output', default='stylish')
     args = gendiff.parse_args()
-    print(generate_diff(args.first_file, args.second_file))
+    dict1 = parse_input_file(args.first_file)
+    dict2 = parse_input_file(args.second_file)
+    diff_dict = generate_diff_dict(dict1, dict2)
+    if args.format == 'stylish':
+        print(format_diff_stylish(diff_dict))
