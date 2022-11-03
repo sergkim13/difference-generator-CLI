f5e83043520955cca8491bc13f3a75cc4b47662f
Sergey Kim <sergkim7@gmail.com>
2022-11-03 21:17:16 +0300

    fixed linter errors

--- gendiff_pack/gendiff_engine/gendiff.py
+++ gendiff_pack/gendiff_engine/gendiff.py
@@ -1,8 +1,6 @@
-from gendiff_pack.gendiff_format.format_diff_stylish import (
-    format_diff_stylish, convert_bool)
+from gendiff_pack.gendiff_format.format_diff_stylish import format_diff_stylish
 import json
 import yaml
-from pprint import pprint
 
 
 def parse_input_file(path_to_file):
@@ -28,27 +26,6 @@ def make_diff_dict(source1, source2):
         else:
             result[(key, 'changed')] = (source1[key], source2[key])
     return result
-
-
-def generate_diff_plain(path_to_file1, path_to_file2):
-    dict1 = parse_input_file(path_to_file1)
-    dict2 = parse_input_file(path_to_file2)
-    keys = sorted(list(dict1.keys() | dict2.keys()))
-    diff_list = []
-    for key in keys:
-        if key not in dict1:
-            diff_list.append(f'+ {key} : {convert_bool(dict2[key])}')
-        elif key not in dict2:
-            diff_list.append(f'- {key} : {convert_bool(dict1[key])}')
-        elif dict1[key] == dict2[key]:
-            diff_list.append(f'  {key} : {convert_bool(dict1[key])}')
-        else:
-            diff_list.append(
-                f'- {key} : {convert_bool(dict1[key])}\n'
-                f'+ {key} : {convert_bool(dict2[key])}')
-    diff_str = '\n'.join(diff_list)
-    result = '{\n' + diff_str + '\n' + '}'
-    return (result)
 
 
 def generate_diff(path_to_file1, path_to_file2):
