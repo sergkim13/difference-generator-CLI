025b25e4d23246651ee38f70c5d94979e205676c
Sergey Kim <sergkim7@gmail.com>
2022-11-04 18:16:51 +0300

    upd gendiff_script_body.py

--- gendiff_pack/gendiff_engine/gendiff_script_body.py
+++ gendiff_pack/gendiff_engine/gendiff_script_body.py
@@ -1,6 +1,5 @@
 import argparse
-from gendiff_pack.gendiff_engine.gendiff import make_diff_dict, parse_input_file
-from gendiff_pack.gendiff_format.format_diff_stylish import format_diff_stylish
+from gendiff_pack import generate_diff
 
 
 def gendiff_script():
@@ -11,8 +10,7 @@ def gendiff_script():
     gendiff.add_argument('-f', '--format', help='set format of output',
                          default='stylish')
     args = gendiff.parse_args()
-    dict1 = parse_input_file(args.first_file)
-    dict2 = parse_input_file(args.second_file)
-    diff_dict = make_diff_dict(dict1, dict2)
-    if args.format == 'stylish':
-        print(format_diff_stylish(diff_dict))
+    path_to_file1 = args.first_file
+    path_to_file2 = args.second_file
+    format_name = args.format
+    print(generate_diff(path_to_file1, path_to_file2, format_name=format_name))
