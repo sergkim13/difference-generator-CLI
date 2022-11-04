6532e59cadca396f067b9e1092f742c648db9eb1
Sergey Kim <sergkim7@gmail.com>
2022-11-04 21:17:49 +0300

    restructure format_diff_stylish.py

--- gendiff_pack/gendiff_format/format_diff_stylish.py
+++ gendiff_pack/gendiff_format/format_diff_stylish.py
@@ -1,27 +1,6 @@
 import itertools
 
 
-def convert_bool(value):
-    if value is True:
-        return 'true'
-    if value is False:
-        return 'false'
-    if value is None:
-        return 'null'
-    return value
-
-
-def get_prefix(key, indent):
-    if key[1] == 'added':
-        return f'{indent}+ {key[0]}: '
-    elif key[1] == 'deleted':
-        return f'{indent}- {key[0]}: '
-    elif key[1] == 'unchanged' or key[1] == 'children':
-        return f'{indent}  {key[0]}: '
-    else:                            # если key не является кортежем,
-        return f'{indent}  {key}: '  # значит val уже не рекурсивный случай
-
-
 def format_diff_stylish(source, replacer=' ', spacesCount=2):
 
     def inner(current_value, depth):
@@ -46,3 +25,24 @@ def format_diff_stylish(source, replacer=' ', spacesCount=2):
         return '\n'.join(result)
 
     return inner(source, 0)
+
+
+def convert_bool(value):
+    if value is True:
+        return 'true'
+    if value is False:
+        return 'false'
+    if value is None:
+        return 'null'
+    return value
+
+
+def get_prefix(key, indent):
+    if key[1] == 'added':
+        return f'{indent}+ {key[0]}: '
+    elif key[1] == 'deleted':
+        return f'{indent}- {key[0]}: '
+    elif key[1] == 'unchanged' or key[1] == 'children':
+        return f'{indent}  {key[0]}: '
+    else:                            # если key не является кортежем,
+        return f'{indent}  {key}: '  # значит val уже не рекурсивный случай
