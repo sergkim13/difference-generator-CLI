213fff0fd2620f9e28486a5aad30a588b573b52c
Sergey Kim <sergkim7@gmail.com>
2022-11-04 20:49:02 +0300

    add test fixtures for json format gendiff

---
+++ tests/test_gendiff_json.py
@@ -0,0 +1,15 @@
+from gendiff_pack import generate_diff
+
+# Плоские файлы
+file1_json = 'tests/fixtures/plain_files/file1.json'
+file2_json = 'tests/fixtures/plain_files/file2.json'
+file1_yml = 'tests/fixtures/plain_files/file1.yml'
+file2_yaml = 'tests/fixtures/plain_files/file2.yaml'
+
+# Вложенные файлы
+file1_nested_json = 'tests/fixtures/nested_files/file1.json'
+file2_nested_json = 'tests/fixtures/nested_files/file2.json'
+file1_nested_yml = 'tests/fixtures/nested_files/file1.yml'
+file2_nested_yaml = 'tests/fixtures/nested_files/file2.yaml'
+
+
