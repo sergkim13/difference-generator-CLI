cb4670c868b168669a4c5315f5b7fb67ca99a922
Sergey Kim <sergkim7@gmail.com>
2022-11-04 17:59:41 +0300

    fixed linter errors

--- gendiff_pack/gendiff_format/format_diff_stylish.py
+++ gendiff_pack/gendiff_format/format_diff_stylish.py
@@ -11,6 +11,17 @@ def convert_bool(value):
     return value
 
 
+def get_prefix(key, indent):
+    if key[1] == 'added':
+        return f'{indent}+ {key[0]}: '
+    elif key[1] == 'deleted':
+        return f'{indent}- {key[0]}: '
+    elif key[1] == 'unchanged' or key[1] == 'children':
+        return f'{indent}  {key[0]}: '
+    else:                            # если key не является кортежем,
+        return f'{indent}  {key}: '  # значит val уже не рекурсивный случай
+
+
 def format_diff_stylish(source, replacer=' ', spacesCount=2):
 
     def inner(current_value, depth):
@@ -22,26 +33,14 @@ def format_diff_stylish(source, replacer=' ', spacesCount=2):
         indent = replacer * indent_size
         lines = []
         for key, val in current_value.items():
-            if key[1] == 'added':
-                lines.append(f'{indent}+ {key[0]}: '
-                             f'{inner(val, indent_size + 2)}')
-            elif key[1] == 'deleted':
-                lines.append(f'{indent}- {key[0]}: '
-                             f'{inner(val, indent_size + 2)}')
-            elif key[1] == 'changed':
-                lines.append(
+            if key[1] == 'changed':  # т.к. при 'changed' выводится 2 строки,
+                lines.append(        # под этот случай выделен отдельный блок if
                     f'{indent}- {key[0]}: '
                     f'{inner(val[0], indent_size + 2)}\n'
                     f'{indent}+ {key[0]}: '
                     f'{inner(val[1], indent_size + 2)}')
-            elif key[1] == 'unchanged':
-                lines.append(f'{indent}  {key[0]}: '
-                             f'{inner(val, indent_size + 2)}')
-            elif key[1] == 'children':
-                lines.append(f'{indent}  {key[0]}: '
-                             f'{inner(val, indent_size + 2)}')
             else:
-                lines.append(f'{indent}  {key}: '
+                lines.append(f'{get_prefix(key, indent)}'
                              f'{inner(val, indent_size + 2)}')
             result = itertools.chain("{", lines, [current_indent + "}"])
         return '\n'.join(result)
