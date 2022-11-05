db70132067ee11a91803ad7dcb4c88a2bd041e61
Sergey Kim <sergkim7@gmail.com>
2022-11-05 12:05:01 +0300

    upd text fixtures and tests for json format gendif

--- tests/test_gendiff_json.py
+++ tests/test_gendiff_json.py
@@ -1,4 +1,5 @@
 from gendiff_pack import generate_diff
+import json
 
 # Плоские файлы
 file1_json = 'tests/fixtures/plain_files/file1.json'
@@ -13,3 +14,19 @@ file1_nested_yml = 'tests/fixtures/nested_files/file1.yml'
 file2_nested_yaml = 'tests/fixtures/nested_files/file2.yaml'
 
 
+def test_generate_diff_json_format_with_plain_files():
+    result = json.load(open('tests/fixtures/results/json_format/json_format_result_with_plain_files.json'))
+    result = json.dumps(result)
+    assert generate_diff(file1_json, file2_json, format_name='json') == result
+    assert generate_diff(file1_yml, file2_yaml, format_name='json') == result
+    assert generate_diff(file1_yml, file2_json, format_name='json') == result
+    assert generate_diff(file1_json, file2_yaml, format_name='json') == result
+
+
+def test_generate_diff_json_format_with_nested_files():
+    result = json.load(open('tests/fixtures/results/json_format/json_format_result_with_nested_files.json'))
+    result = json.dumps(result)
+    assert generate_diff(file1_nested_json, file2_nested_json, format_name='json') == result
+    assert generate_diff(file1_nested_yml, file2_nested_yaml, format_name='json') == result
+    assert generate_diff(file1_nested_yml, file2_nested_json, format_name='json') == result
+    assert generate_diff(file1_nested_json, file2_nested_yaml, format_name='json') == result
