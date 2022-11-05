35bcd1297c1b57af42292eec5a52106068fcbd56
Sergey Kim <sergkim7@gmail.com>
2022-11-05 12:51:00 +0300

    upd gendiff_script_body.md

--- gendiff_pack/gendiff_engine/gendiff_script_body.py
+++ gendiff_pack/gendiff_engine/gendiff_script_body.py
@@ -7,8 +7,12 @@ def gendiff_script():
         description='Compares two configuration files and shows a difference.')
     gendiff.add_argument('first_file')
     gendiff.add_argument('second_file')
-    gendiff.add_argument('-f', '--format', help='set format of output',
-                         default='stylish')
+    gendiff.add_argument('-f', '--format', help='set format of output. '
+                         'Available output formats:'
+                         'stylish (default formatter); '
+                         'plain (-f plain); '
+                         'json (-f json)', default='stylish',
+                         choices=['stylish', 'plain', 'json'])
     args = gendiff.parse_args()
     path_to_file1 = args.first_file
     path_to_file2 = args.second_file
