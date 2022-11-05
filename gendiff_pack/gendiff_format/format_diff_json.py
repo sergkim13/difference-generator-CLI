d16786669635895d77fea21d8644a611ea7d6309
Sergey Kim <sergkim7@gmail.com>
2022-11-05 12:05:16 +0300

    upd format_diff_json.py

---
+++ gendiff_pack/gendiff_format/format_diff_json.py
@@ -0,0 +1,31 @@
+import json
+
+
+def format_diff_json(source):
+
+    def inner(current_value):
+        if not isinstance(current_value, dict):
+            return current_value
+
+        json_dict = {}
+        for key, val in current_value.items():
+            if key[1] == 'changed':
+                json_dict[f'- {key[0]}'] = inner(val[0])
+                json_dict[f'+ {key[0]}'] = inner(val[1])
+            else:
+                json_dict[convert_keys(key)] = inner(val)
+        return json_dict
+
+    result = inner(source)
+    return json.dumps(result)
+
+
+def convert_keys(key):
+    keys_dict = {
+        'added': "+ ",
+        'deleted': '- ',
+        'changed': '',
+        'unchanged': '',
+        'children': ''
+    }
+    return f'{keys_dict[key[1]]}{key[0]}' if isinstance(key, tuple) else key
